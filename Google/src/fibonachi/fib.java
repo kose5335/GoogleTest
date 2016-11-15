47dcf54d41211bccf5185e366327644efa7b7d78
kose5335 <kose5335@gmail.com>
2016-11-15 23:19:28 +0200

    fibonachi

---
+++ Google/src/fibonachi/fib.java
@@ -0,0 +1,20 @@
+package fibonachi;
+
+/**
+ * Created by Sergey on 11/15/2016.
+ */
+public class fib {
+    public static void main(String[] args) throws Exception
+    {
+        int n1=0, n2=1, n3=0;
+        int sum=0;
+        System.out.println("1="+n1);
+        System.out.println("2="+n2);
+        for (int i=1; i<=28; i++) {
+            n3=n1+n2;
+            System.out.println((i+2)+"="+n3);
+            n1=n2; n2=n3;
+        }
+    }
+
+}
