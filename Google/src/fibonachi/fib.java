2ab72b8e59c587b4a50ad6f3327cd9fca4368004
kose5335 <kose5335@gmail.com>
2016-11-16 19:58:57 +0200

    List<WebElement>

--- Google/src/fibonachi/fib.java
+++ Google/src/fibonachi/fib.java
@@ -7,7 +7,7 @@ public class fib {
     public static void main(String[] args) throws Exception
     {
         int n1=0, n2=1, n3=0;
-        int sum=0;
+        //int sum=0;
         System.out.println("1="+n1);
         System.out.println("2="+n2);
         for (int i=1; i<=28; i++) {
--- Google/src/test1/webtest.java
+++ Google/src/test1/webtest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.List;
+
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 
@@ -24,13 +27,19 @@ public class webtest {
         driver.findElement(By.name("q")).sendKeys("Кононенко Сергей Владимирович");
         driver.findElement(By.name("btnG")).click();
         driver.manage().timeouts().implicitlyWait(3, SECONDS);
-        for (int i=1;i<=10; i++) {
-         System.out.println(driver.findElement(By.xpath("(//h3[@class='r']//a)["+i+"]")).getAttribute("href"));
-        }
+        List<WebElement> links=driver.findElements(By.xpath("//h3[@class='r']//a"));
+        System.out.println("Total element is "+links.size());
+        Iterator<WebElement> i1=links.iterator();
+            for (int i=1; i <=5; i++) {
+                System.out.println(i+" link: "+i1.next().getAttribute("href"));
+            }
+        //for (int i=1;i<=10; i++) {
+         //System.out.println(driver.findElement(By.xpath("(//h3[@class='r']//a)["+i+"]")).getAttribute("href"));
+        //}
         //WebElement searchResult1 = driver.findElement(By.xpath("(//h3[@class='r']//a)[1]"));
         //String searchResult1Text = searchResult1.getAttribute("href");
         //System.out.println(driver.findElement(By.xpath("(//h3[@class='r']//a)[1]")).getAttribute("href"));
-
+        // //h3[@class='r']//a
         driver.close();
         //if (searchResult1Text.contains("КОНОНЕНКО СЕРГЕЙ ВЛАДИМИРОВИЧ"))
             //System.out.println("true");
