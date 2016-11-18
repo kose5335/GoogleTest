d27a0f33d57b71167456552ade9124d9749c8577
kose5335 <kose5335@gmail.com>
2016-11-18 19:01:19 +0200

    New version

--- Google/src/AutomationPractice/AngelNetCheck.java
+++ Google/src/AutomationPractice/AngelNetCheck.java
@@ -17,52 +17,29 @@ public class AngelNetCheck {
         System.setProperty("webdriver.chrome.driver", "D://Tester/GoogleDriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://angel.net/~nic/passwd.current.html");
-        String mp="P@ssw0rd";
+        String mp="P@ssw0rd",site="vk.com";
         for (int i=1; i<=10; i++) {
-            driver.findElement(By.name("site")).clear();
-            driver.findElement(By.name("master")).clear();
-            driver.findElement(By.name("master")).sendKeys(mp);
-            driver.findElement(By.name("site")).sendKeys("vk" + i + ".com");
-            driver.findElement(By.xpath("//input[@type='submit']")).submit();
-            //driver.manage().timeouts().implicitlyWait(1, SECONDS);
-            System.out.println("The password for the site \"vk" + i + ".com\" is " + driver.findElement(By.name("password")).getAttribute("value"));
+            System.out.println("The password for the site \"vk"+i+".com\" is "+GetPass(driver, mp, "vk" + i + ".com"));
         }
-        boolean test=false;
-        String p1,p2;
-        driver.findElement(By.name("site")).clear();
-        driver.findElement(By.name("master")).clear();
-        driver.findElement(By.name("master")).sendKeys(mp);
-        driver.findElement(By.name("site")).sendKeys("vk.com");
-        driver.findElement(By.xpath("//input[@type='submit']")).submit();
-        p1=driver.findElement(By.name("password")).getAttribute("value");
-        for (int i=1;i<=100;i++) {
-            driver.findElement(By.name("site")).clear();
-            driver.findElement(By.name("master")).clear();
-            driver.findElement(By.name("master")).sendKeys(mp);
-            driver.findElement(By.name("site")).sendKeys("vk.com");
-            driver.findElement(By.xpath("//input[@type='submit']")).submit();
-            p2=driver.findElement(By.name("password")).getAttribute("value");
-            test=p2.equals(p1);
-            p1=p2;
-        }
-        if (test) {
+        String p1=GetPass(driver, mp, site);
+        int TrueCount=0, Cases=100;
+        for (int i=1;i<=Cases;i++) if (p1.equals(GetPass(driver, mp, site))) TrueCount++;
+        if (TrueCount==Cases) {
             System.out.println("Site is pretty good");
         }
-        else System.out.println("Site is bad");
+        else {
+            System.out.println("Site is bad");
+        }
         driver.close();
-        /*
-               WebElement element = (WebElement) ((JavascriptExecutor) driver)
-               .executeScript("return document.getElementById('gbqfq')");
-               element.sendKeys("search text");
-               System.out.println(element.getTagName());
-               */
-
     }
-    //public String GetPass(String pass) {
-       // driver.findElement(By.name("site")).clear();
-       // driver.findElement(By.name("master")).clear();
-       // driver.findElement(By.name("master")).sendKeys(this.mp);
-        //driver.findElement(By.name("site")).sendKeys("vk.com");
-        //driver.findElement(By.xpath("//input[@type='submit']")).submit();
-
+    private static String GetPass(WebDriver driver, String pass, String SiteName) {
+        driver.findElement(By.name("master")).clear();
+        driver.findElement(By.name("site")).clear();
+        driver.findElement(By.name("master")).sendKeys(pass);
+        driver.findElement(By.name("site")).sendKeys(SiteName);
+        driver.findElement(By.xpath("//input[@type='submit']")).submit();
+        return driver.findElement(By.name("password")).getAttribute("value");
+    }
 }
+
+
